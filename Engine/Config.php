42e54ccc76f54c8d68e6523bc535437f81c8f2e7
Colinleg <80259244+colinleg@users.noreply.github.com>
2021-07-30 17:02:50 +0200

    IC

---
+++ Engine/Config.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace BlogPhp\Engine;
+
+final class Config
+{
+    // Identifiants Database
+    const
+    DB_HOST = 'synolego.synology.me:44132',
+    DB_NAME = 'blog-ecrivain',
+    DB_USR = 'dwwm',
+    DB_PWD = 'dwwm21',
+
+    // Titre du site
+    SITE_NAME = 'Billet simple pour l\'Alaska';
+}
